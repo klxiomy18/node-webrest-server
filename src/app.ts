c944535b47bbee4a76f3f1d2c059f49472709e84
XiomaraRivera20 <kristin.munoz@cdtcreatic.com>
2024-09-07 15:27:48 -0500

    First commit

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import { envs } from './config/envs';
+import {Server} from './presentation/server'
+
+(async()=> {
+
+    main()
+
+})();
+
+export function main(){
+
+    const server = new Server({
+        port: envs.PORT,
+        publicPath: envs.PUBLIC_PATH
+    });
+
+    server.start()
+    
+}

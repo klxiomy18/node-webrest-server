ab2e67a3725c91a80e62b9aff0a47f23a186ac59
XiomaraRivera20 <kristin.munoz@cdtcreatic.com>
2024-09-11 15:51:25 -0500

    Last section finished

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,10 @@
-import express from  'express'
+import express, { Router } from  'express'
 import path from 'path';
 
 interface Options{
     port:number,
-    publicPath?:string
+    publicPath?:string,
+    routes: Router
 }
 
 
@@ -12,19 +13,29 @@ export class Server {
     private app = express();
     private readonly port: number;
     private readonly publicPath: string;
+    private readonly routes: Router;
+
 
     constructor(options:Options){
-        const {port, publicPath = 'public'} = options;
+        const {port, routes, publicPath = 'public'} = options;
         this.port = port;
         this.publicPath = publicPath
-
+        this.routes = routes;
     }
 
     async start(){
 
-        //Middlewares
+        //* Middlewares
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended:true}))
+
+        //* Public folder
         this.app.use(express.static(this.publicPath));
 
+        //*Routes
+        this.app.use(this.routes);
+
+        //* SPA
         this.app.get('*', (req,res)=> {
             const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`);
             res.sendFile(indexPath);
---
+++ src/presentation/todos/contoller-ddd.ts
@@ -0,0 +1,60 @@
+import { Request, Response } from "express";
+import { prisma } from "../../data/postgres";
+import { CreateTodoDto, UpdateTodoDto } from "../../domain/dtos";
+import { TodoRepository } from "../../domain";
+
+// const todos = [
+//     {id:1, text:'Buy milk', completedAt: new Date()},
+//     {id:2, text:'Buy potatoes', completedAt: null},
+
+// ];
+
+export class TodoController {
+
+    constructor(
+        private readonly todoRepository: TodoRepository
+    ){}
+
+    public getTodos = async (req:Request, res:Response) => {
+        const todos = await this.todoRepository.getAll();
+        return res.json(todos)
+    }
+
+    public getTodoById = async (req:Request, res:Response) => {
+        const id =+ req.params.id;
+
+        try {
+            const todo = await this.todoRepository.findById(id);
+            res.json(todo)
+        } catch (error) {
+            res.status(404).json(error)
+        }  
+    }
+
+    public createTodo = async (req:Request, res:Response) => {
+
+        const [error, createTodoDto] = CreateTodoDto.create(req.body);
+        if(error)return res.status(400).json({error});
+
+        const todo = await this.todoRepository.create(createTodoDto!);
+        return res.json(todo);
+    }
+    public updateTodo = async  (req:Request, res:Response) => {
+
+        const id =+ req.params.id;
+        const [error, updateTodoDto] = UpdateTodoDto.create({...req.body, id});
+        if(error) return res.status(400).json({error})
+
+        const updateTodo = await this.todoRepository.updateById(updateTodoDto!);
+        return res.json(updateTodo)
+
+    }
+
+    public deleteTodo = async (req:Request, res:Response) => {
+
+        const id =+ req.params.id;
+        const deleteTodo = await this.todoRepository.deleteById(id);
+        res.json(deleteTodo);
+    }
+
+}
